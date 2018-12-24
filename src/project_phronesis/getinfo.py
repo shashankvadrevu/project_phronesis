aa463a49fce034d426010f9c05a521cefa23373d
BlacknMellow <shreesha.addala2012@vit.ac.in>
2018-12-24 14:26:24 -0500

    Parse all JSON fields

--- src/project_phronesis/getinfo.py
+++ src/project_phronesis/getinfo.py
@@ -5,6 +5,8 @@ import json
 import re
 import sys
 from bs4 import BeautifulSoup
+import pdb
+import re
 
 
 def parse_response(path):
@@ -12,13 +14,18 @@ def parse_response(path):
         recall_list = ast.literal_eval(f.read())
 
     for each in recall_list:
-        recall_url = "http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/{}/en".format(
+        recall_url = "http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/68624/en".format(
             each)
         r = requests.get(recall_url)
         j_soup = json.loads(r.text)
+        basic_details = parse_basicDetails(j_soup)
+        panel_details = parse_panel_details(j_soup)
+        product_details = parse_product_details(j_soup)
+        other_details = parse_other_details(j_soup)
+        image_details = parse_image_details(j_soup)
         break
 
-    return j_soup
+    return "execution complete"
 
 
 def parse_basicDetails(j_soup):
@@ -39,18 +46,31 @@ def parse_basicDetails(j_soup):
     return basic_details_dict
 
 
-def pasre_cms_who_what_consumer(j_soup):
-    cms_who_what_consumer_dict = {}
-    for each in j_soup['panels']:
-        if each.get('panelName') == 'cms_who_what_consumer':
-            html = each.get('text')
-            html_strip = html.replace("\n", "")
-            soup = BeautifulSoup(html_strip, "html5lib")
-            key1 = 'cms_who_what_consumer'
-            value1 = soup.get_text()
-            cms_who_what_consumer_dict.update({key1: value1})
+# def parse_cms_who_what_consumer(j_soup):
+#     cms_who_what_consumer_dict = {}
+#     for each in j_soup['panels']:
+#         if each.get('panelName') == 'cms_who_what_consumer':
+#             html = each.get('text')
+#             html_strip = html.replace("\n", "")
+#             soup = BeautifulSoup(html_strip, "html5lib")
+#             key1 = 'cms_who_what_consumer'
+#             value1 = soup.get_text()
+#             cms_who_what_consumer_dict.update({key1: value1})
 
-    return cms_who_what_consumer_dict
+#     return cms_who_what_consumer_dict
+
+def parse_panel_details(j_soup):
+    panel_details = {}
+    regex_str = 'product_[0-9]+|basic_details|images'
+    pattern = re.compile(regex_str)
+    for panel in j_soup['panels']:
+        if not pattern.match(panel["panelName"]):
+            html_content = panel.get('text')
+            html_strip = html_content.replace("\n", "")
+            soup = BeautifulSoup(html_strip, "html5lib")
+            value = soup.get_text()
+            panel_details[panel['panelName']] = value
+    return panel_details
 
 
 def parse_product_details(j_soup):
@@ -84,15 +104,41 @@ def parse_product_details(j_soup):
     return product_dict
 
 
+def parse_other_details(j_soup):
+    other_details = {}
+    keys_list = ['url', 'recallId', 'title', 'start_date', 'date_published']
+    for key in keys_list:
+        if key in j_soup:
+            other_details[key] = j_soup[key]
+
+    return other_details
+
+
+def parse_image_details(j_soup):
+    base_url = 'http://healthycanadians.gc.ca'
+    image_details = {}
+    for panel in j_soup["panels"]:
+        if panel["panelName"] == "images":
+            for i, img in enumerate(panel["data"]):
+                image_details['img_' + str(i+1)] = {'title': img["title"],
+                                                    'fullUrl': base_url+img["fullUrl"]}
+    return image_details
+
+
 if __name__ == '__main__':
 
+    # path = str(sys.argv[1])
+    # # path = 'D:/project_phronesis/docs/files/recall_list.txt'
+    # json_respone = parse_response(path)
+    # # pprint.pprint(json_respone)
+    # basic_details = parse_basicDetails(json_respone)
+    # who_what = parse_cms_who_what_consumer(json_respone)
+    # # pprint.pprint((who_what))
+    # product_details = parse_product_details(json_respone)
+
+    # pprint.pprint(product_details)
+
     path = str(sys.argv[1])
-    # path = 'D:/project_phronesis/docs/files/recall_list.txt'
-    json_respone = parse_response(path)
-    # pprint.pprint(json_respone)
-    basic_details = parse_basicDetails(json_respone)
-    who_what = pasre_cms_who_what_consumer(json_respone)
-    # pprint.pprint((who_what))
-    product_details = parse_product_details(json_respone)
+    product_details = parse_response(path)
 
     pprint.pprint(product_details)
