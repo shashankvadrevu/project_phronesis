a67194edb771d833c004d0717b587122b8711fe0
shashankvadrevu <shashank.vadrevu@outlook.com>
2018-12-26 18:18:58 -0500

    Modularized code removed file parsing added a seperate script

--- src/project_phronesis/getinfo.py
+++ src/project_phronesis/getinfo.py
@@ -2,143 +2,67 @@ import ast
 import pprint
 import requests
 import json
-import re
 import sys
-from bs4 import BeautifulSoup
 import pdb
-import re
+import random
+import parseResponse
+import time
+import datetime
 
 
 def parse_response(path):
+
     with open(path, 'r') as f:
         recall_list = ast.literal_eval(f.read())
+        random.seed(4)
+        recall_list_200 = random.sample(recall_list, 10)
+
+    list_response = []
+    recall_details_dict = {}
 
     for each in recall_list:
-        recall_url = "http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/68624/en".format(
+        recall_url = "http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/{}/en".format(
             each)
         r = requests.get(recall_url)
         j_soup = json.loads(r.text)
-        basic_details = parse_basicDetails(j_soup)
-        panel_details = parse_panel_details(j_soup)
-        product_details = parse_product_details(j_soup)
-        other_details = parse_other_details(j_soup)
-        image_details = parse_image_details(j_soup)
-        break
 
-    return "execution complete"
+        basic_details = parseResponse.parse_basicDetails(j_soup)
+        panel_details = parseResponse.parse_panel_details(j_soup)
+        product_details = parseResponse.parse_product_details(j_soup)
+        other_details = parseResponse.parse_other_details(j_soup)
+        image_details = parseResponse.parse_image_details(j_soup)
+        basic_details.update(other_details)
 
+        start_date = basic_details.get("start_date")
+        start_date = time.strftime(
+            "%a %d %b %Y %H:%M:%S", time.gmtime(start_date))
+        basic_details["start_date"] = start_date
 
-def parse_basicDetails(j_soup):
-    for each in j_soup['panels']:
-        if each.get('panelName') == 'basic_details':
-            html = each.get('text')
-            basic_details = html.split("<BR/>")
-            basic_details_dict = {}
-            for each in basic_details:
-                try:
-                    basic_each = each.split("</b>")
-                    key1 = (basic_each[0].replace("<b>", ""))
-                    key1 = key1.replace(":", "")
-                    value1 = basic_each[1].lstrip(" ")
-                    basic_details_dict.update({key1: value1})
-                except Exception as e:
-                    pass
-    return basic_details_dict
+        parsed_response = {}
+        parsed_response.update({"basic_details": basic_details})
+        parsed_response.update({"panel_details": panel_details})
+        parsed_response.update({"product_details": product_details})
+        # parsed_response.update({"other_details": other_details})
+        parsed_response.update({"image_details": image_details})
 
+        key1 = each
+        value1 = dict(parsed_response)
 
-# def parse_cms_who_what_consumer(j_soup):
-#     cms_who_what_consumer_dict = {}
-#     for each in j_soup['panels']:
-#         if each.get('panelName') == 'cms_who_what_consumer':
-#             html = each.get('text')
-#             html_strip = html.replace("\n", "")
-#             soup = BeautifulSoup(html_strip, "html5lib")
-#             key1 = 'cms_who_what_consumer'
-#             value1 = soup.get_text()
-#             cms_who_what_consumer_dict.update({key1: value1})
+        recall_details_dict.update({key1: value1})
 
-#     return cms_who_what_consumer_dict
+        list_response.append(dict(recall_details_dict))
 
-def parse_panel_details(j_soup):
-    panel_details = {}
-    regex_str = 'product_[0-9]+|basic_details|images'
-    pattern = re.compile(regex_str)
-    for panel in j_soup['panels']:
-        if not pattern.match(panel["panelName"]):
-            html_content = panel.get('text')
-            html_strip = html_content.replace("\n", "")
-            soup = BeautifulSoup(html_strip, "html5lib")
-            value = soup.get_text()
-            panel_details[panel['panelName']] = value
-    return panel_details
+        # break
 
+    # with open('recall_randon_200.txt', 'w') as outfile:
+    #     json.dump(list_response, outfile)
 
-def parse_product_details(j_soup):
-    basic_product_dict = {}
-    pattern = "product_"
-    product_details = []
-    product_dict = {}
-    # product_list = []
-    for each in j_soup["panels"]:
-        if pattern in each.get('panelName') and each.get('panelName') != 'cms_product_details_background':
-            html = each.get('text')
-            basic_details = html.split("<BR/>")
-            product_details.append(basic_details)
-
-    for i, each in enumerate(product_details):
-        for subEach in each:
-            try:
-                basic_each = subEach.split("</b>")
-                key1 = (basic_each[0].replace("<b>", ""))
-                key1 = key1.replace(":", "")
-                value0 = basic_each[1].lstrip(" ")
-                soup = BeautifulSoup(value0, "html5lib")
-                value1 = soup.get_text()
-                basic_product_dict.update({key1: value1})
-            except Exception as e:
-                pass
-    # product_list.append(dict(basic_product_dict))
-        key2 = "product_{}".format(i + 1)
-        product_dict.update({key2: dict(basic_product_dict)})
-
-    return product_dict
-
-
-def parse_other_details(j_soup):
-    other_details = {}
-    keys_list = ['url', 'recallId', 'title', 'start_date', 'date_published']
-    for key in keys_list:
-        if key in j_soup:
-            other_details[key] = j_soup[key]
-
-    return other_details
-
-
-def parse_image_details(j_soup):
-    base_url = 'http://healthycanadians.gc.ca'
-    image_details = {}
-    for panel in j_soup["panels"]:
-        if panel["panelName"] == "images":
-            for i, img in enumerate(panel["data"]):
-                image_details['img_' + str(i+1)] = {'title': img["title"],
-                                                    'fullUrl': base_url+img["fullUrl"]}
-    return image_details
+    return list_response
 
 
 if __name__ == '__main__':
-
-    # path = str(sys.argv[1])
-    # # path = 'D:/project_phronesis/docs/files/recall_list.txt'
-    # json_respone = parse_response(path)
-    # # pprint.pprint(json_respone)
-    # basic_details = parse_basicDetails(json_respone)
-    # who_what = parse_cms_who_what_consumer(json_respone)
-    # # pprint.pprint((who_what))
-    # product_details = parse_product_details(json_respone)
-
-    # pprint.pprint(product_details)
-
     path = str(sys.argv[1])
-    product_details = parse_response(path)
-
-    pprint.pprint(product_details)
+    recall_details_list = parse_response(path)
+    # print()
+    pprint.pprint(recall_details_list)
+    # pprint.pprint(dateConvert(recall_details_list))
